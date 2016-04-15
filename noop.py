86359620a0eabd8caeabababbdd080c5f804502f
Sargun Dhillon <sargun@sargun.me>
2016-04-15 09:43:43 -0700

    Checkpoint.

---
+++ noop.py
@@ -0,0 +1,22 @@
+import gnlpy.netlink as netlink
+
+AttrList = netlink.create_attr_list_type('Minuteman',
+  ('MINUTEMAN_ATTR_VIP_IP', netlink.U32Type),
+  ('MINUTEMAN_ATTR_VIP_PORT', netlink.U16Type),
+  ('MINUTEMAN_ATTR_BE_IP', netlink.U32Type),
+  ('MINUTEMAN_ATTR_BE_PORT', netlink.U16Type),
+  ('MINUTEMAN_ATTR_BE_AVAIL', netlink.U8Type),
+)
+
+MinutemanMessage = netlink.create_genl_message_type(
+  'Minuteman', 'MINUTEMAN',
+  ('MINUTEMAN_CMD_NOOP', AttrList),
+  ('MINUTEMAN_CMD_ADD_VIP', AttrList),
+  ('MINUTEMAN_CMD_DEL_VIP', AttrList),
+  ('MINUTEMAN_CMD_SET_BE', AttrList),
+  ('MINUTEMAN_CMD_ADD_BE', AttrList),
+  ('MINUTEMAN_CMD_DEL_BE', AttrList),
+)
+
+sock = netlink.NetlinkSocket()
+print sock.query(TaskstatsMessage('MINUTEMAN_CMD_NOOP', attr_list=AttrList()))
--- test.c
+++ test.c
@@ -11,8 +11,13 @@
 #include <netlink/genl/ctrl.h>
 
 #include "minuteman.h"
+static int parse_cb(struct nl_msg *msg, void *arg)
+{
+  printf("pants\n");
+}
 
 int main() {
+  int err;
   struct nl_sock *sk = nl_socket_alloc();
   if (!sk) {
     fprintf(stderr, "Could not allocate socket\n");
@@ -31,12 +36,37 @@ int main() {
     goto fail;
   }
 
+/*
   ret = genl_send_simple(sk, family, MINUTEMAN_CMD_NOOP, 1, NLM_F_DUMP);
   printf("Status: %d\n", ret);
 
   ret = genl_send_simple(sk, family, MINUTEMAN_CMD_ADD_VIP, 1, NLM_F_DUMP);
   printf("Status: %d\n", ret);
+*/
+  struct nl_msg *msg;
+  if (!(msg = nlmsg_alloc()))
+    exit(1);
+
+  void *hdr;
+  hdr = genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, family, 0, 0, MINUTEMAN_CMD_ADD_VIP, 1);
+  if (!hdr)
+    exit(1);
+  struct in_addr addr;
+  inet_aton("1.2.3.4", &addr);
+
+  nla_put_u32(msg, MINUTEMAN_ATTR_VIP_IP, addr.s_addr);
+  nla_put_u16(msg, MINUTEMAN_ATTR_VIP_PORT, htons(1111));
+  nl_send_auto_complete(sk, msg);
+
+  if ((err = nl_recvmsgs_default(sk)) < 0)
+                fprintf(stderr, "Unable to receive message: %s", nl_geterror(err));
+
+  ret = genl_send_simple(sk, family, MINUTEMAN_CMD_NOOP, 1, 0);
+  printf("Status: %d\n", ret);
+
   nl_socket_free(sk);
+
+
   return 0;
   fail:
   nl_socket_free(sk);
